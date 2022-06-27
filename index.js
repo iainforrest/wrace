e3eebbee423329a0df9c23b97bd126b1ef6ce690
Iain Forrest <none>
2022-06-27 21:10:49 +1200

    refactor countdown timer into own function

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var countdown = 15;
 var startGame = 0
 
-
+function countdownTimer(countdown) {
+  var x = setInterval(function() {
+    $("#count").text(countdown)
+    countdown--;
+    if (countdown == -1) {
+      $("#count").text("Game Over")
+      clearInterval(x);
+    }
+  },1000);
+}
 
 
 
 $(document).keydown(function(){
   if (startGame == 0) {
     startGame = 1;
-    var x = setInterval(function() {
-      $("#count").text(countdown)
-      countdown--;
-      if (countdown == -1) {
-        $("#count").text("Game Over")
-        clearInterval(x);
-      }
-    },1000);
-
+    countdownTimer(15);
   }
 })
