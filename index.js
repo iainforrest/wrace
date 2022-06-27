4a66a1cd0a1aaecef750edbbc28a6f2e12d39fbb
Iain Forrest <none>
2022-06-27 21:04:09 +1200

    create countdown timer

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var countdown = 15;
+var startGame = 0
 
+
+
+
+
+$(document).keydown(function(){
+  if (startGame == 0) {
+    startGame = 1;
+    var x = setInterval(function() {
+      $("#count").text(countdown)
+      countdown--;
+      if (countdown == -1) {
+        $("#count").text("Game Over")
+        clearInterval(x);
+      }
+    },1000);
+
+  }
+})
