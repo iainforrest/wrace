f665696b67e2bec4e8378a51dc1dde709dd5688a
Iain Forrest <iainforrest@me.com>
2022-07-02 13:21:47 +1200

    basic gameplay progressed

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const keyboardLetters = [['q','w','e','r','t','y','u','i','o','p'],['a','s','d','f','g','h','j','k','l'],['⌫','z','x','c','v','b','n','m','↲']]
 const numberOfGuesses = 5;
 const timerLength = 14;
+const secretWord = "mined";
+
+var correctLetters = [4,2,1];
+var lettersLeft = [];
+var attempt = 0;
+var currentRow;
+var nextLetterBox;
 
 var startGame = 0;
 var letterBoxRowHTML = '<div class="letterBoxRow"></div>';
@@ -8,7 +15,12 @@ var letterBoxHTML = "<div class='letterBox' data-type='empty'></div>";
 var keyboardRowHTML = '<div class="keyboardRow"></div>';
 var buttonKeyHTML = "<button type='button' data-key='' class='keyBtn'></button>"
 
-var nextLetter = [0,1,2,3,4];
+for (i=0; i<5; i++){
+  if (correctLetters.indexOf(i) == -1) {
+      lettersLeft.push(i);
+  }
+}
+
 
 function gameOver(){
   $("#count").text("Game Over");
@@ -35,8 +47,6 @@ for (i=0; i<numberOfGuesses; i++) {- // 5 rows
 
 }
 
-var nextLetterBox = $('.letterBox:empty').first(); //gets next box with nothing in it
-
 //keyboard
 for (i=0; i<3; i++){ // 3 rows
   $('.keyboard').append(keyboardRowHTML); //append row
@@ -51,33 +61,87 @@ for (i=0; i<3; i++){ // 3 rows
 }
 
 
+//Load hints onto the current row
+function loadGameBoardRow() {
+  currentRow = $('.letterBoxRow').eq(attempt);
+  correctLetters.forEach(function (position){
+    currentRow.children().eq(position).addClass("correct").text(secretWord[position]);
+  });
+  nextLetterBox = $('.letterBox:empty').first();
+}
 
+//check if letter exists in more than one place
+function getAllIndexes(arr, val) {
+  var indexes = [], i;
+  for(i = 0; i < arr.length; i++)
+      if (arr[i] === val)
+          indexes.push(i);
+  return indexes;
+  }
+
+//check if word is correct on enter
+function checkWord() {
+  let lettersToCheck = lettersLeft;
+  lettersToCheck.forEach(function(value,index,array) {
+    let letterToCheck = currentRow.children().eq(value).html();
+    let instances = getAllIndexes(secretWord, letterToCheck);//check for multiple letters
+    if (instances.length == 0) {
+      return;
+    } else if (instances.length == 1){ //letter is in word one time
+        if (instances[0] == value){
+          //correct position
+          console.log("correct position");
+          lettersLeft.splice(index,1);
+          correctLetters.push(value);
+        }else {
+        // correct letter wrong position
+        console.log("correct letter, wrong position");
+        }
+    } else {
+      //multiple instances
+    }
+  });
+
+}
+
+//start game
+$('#count').click(function(){
+  if (startGame == 0) { //start timer if game hasn't started
+    startGame = 1;
+    $('#count').text(timerLength+1).removeClass("startButton");
+    countdownTimer(timerLength);
+    loadGameBoardRow();
+  }
+})
+
+
+//event listner for keyboard presss then take action
 $('.keyBtn').click(function(){
-  if (startGame == 1){
-    let keyPressed = $(this).attr("data-key")
+  if (startGame == 1){ // do nothing if the countdown hasn't startedd
+    let keyPressed = $(this).attr("data-key") //get the key
+    //case statement del, enter, other letter
     switch (keyPressed) {
       case '⌫':
         alert('backspace pressed');
         break;
       case '↲':
           alert('enter pressed');
+          checkWord();
+          if (lettersLeft.length > 0) {
+            attempt ++;
+            loadGameBoardRow();
+          }else {
+            alert("you win")
+          }
+
         break;
       default:
-        nextLetterBox.text(keyPressed);
-        nextLetterBox = $('.letterBox:empty').first();
+        if (nextLetterBox.parent()[0] == currentRow[0]){ //only enter text if the next empty box is on the current row
+          nextLetterBox.text(keyPressed);
+          nextLetterBox = $('.letterBox:empty').first();
+        }
+
     }
   }
 
 });
-
-
-
-
-
-$('#count').click(function(){
-  if (startGame == 0) { //start timer if game hasn't started
-    startGame = 1;
-    $('#count').text(timerLength+1).removeClass("startButton");
-    countdownTimer(timerLength);
-  }
-})
